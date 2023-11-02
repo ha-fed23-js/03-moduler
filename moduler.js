a06a4d2683d05c2090f06fe6ed0b2ad43606a777
David Andersson <david.andersson@zocom.se>
2023-11-02 10:33:46 +0100

    Bankfunktion

--- moduler.js
+++ moduler.js
@@ -1,17 +1,23 @@
-let balance = 25000  // saldot på kontot
-let interest = 1.05  // 5% ränta
 let years = 5
 
-// Efter 5 år:
-for( let i=0; i < years; i++ ) {
-	balance = balance * interest
-	console.log(`År ${i + 1} har vi ${balance} kr på kontot.`);
+function calculateBalance() {
+	let balance = 25000  // saldot på kontot
+	let interest = 1.05  // 5% ränta
+	// Efter 5 år:
+	for( let i=0; i < years; i++ ) {
+		balance = balance * interest
+		console.log(`År ${i + 1} har vi ${balance} kr på kontot.`);
+	}
+	return balance
 }
+
+// Alternativ loop, med while i stället för for
 // let i = 0
 // while ( i < years ) {
 // 	balance = balance * interest
 // 	i++
 // }
 
-let balanceRounded = Math.round(balance)
+let customerBalance = calculateBalance()
+let balanceRounded = Math.round(customerBalance)
 console.log(`Efter ${years} år finns det ${balanceRounded} kr på kontot.`);
